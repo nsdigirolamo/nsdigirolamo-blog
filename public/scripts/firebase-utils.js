91c578f3026b293aaba91ec06d7db6109ebb8d95
nsdigirolamo <nsdigirolamo@gmail.com>
2023-10-24 16:05:33 -0400

    Added simple function that pulls text from stored files.

---
+++ public/scripts/firebase-utils.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBt3H6-xJoSpjTFR3yPlo77fixKO5zrwZs",
+    authDomain: "nsdigirolamo-blog.firebaseapp.com",
+    databaseURL: "https://nsdigirolamo-blog-default-rtdb.firebaseio.com",
+    projectId: "nsdigirolamo-blog",
+    storageBucket: "nsdigirolamo-blog.appspot.com",
+    messagingSenderId: "150745418782",
+    appId: "1:150745418782:web:dca6dd5db998aa2b41b20f",
+    measurementId: "G-RCX63BZ8XT"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
--- public/scripts/pages/projects.js
+++ public/scripts/pages/projects.js
@@ -1,4 +1,7 @@
+import { storage } from "../firebase-utils.js";
 import { getPageView } from "../routing-utils.js";
+
+import { ref, list, getBlob } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
 
 /**
  * Loads the elements required for the projects page.
@@ -6,7 +9,18 @@ import { getPageView } from "../routing-utils.js";
  */
 export function loadProjectsElements (path_segments) {
     getPageView().innerHTML = `
-        <h1>Nicholas DiGirolamo</h1>
-        <h2>Projects</h2>
+        <h1>Projects</h1>
+        <div id="project-list">
+        <div>
     `;
+    getPosts();
+}
+
+async function getPosts () {
+    const posts = (await list(ref(storage, "/project-posts"))).items;
+    for (const storage_reference of posts) {
+        getBlob(storage_reference)
+            .then(blob_result => blob_result.text())
+            .then(text_result => console.log(text_result));
+    }
 }
