6460b81c4b4a32b9ab69209835d22b7365060705
nsdigirolamo <nsdigirolamo@gmail.com>
2023-10-25 19:27:03 -0400

    Added simple system to retrieve files from storage and display them as cards

--- public/scripts/pages/projects.js
+++ public/scripts/pages/projects.js
@@ -13,14 +13,64 @@ export function loadProjectsElements (path_segments) {
         <div id="project-list">
         <div>
     `;
-    getPosts();
+    getPosts("/project-posts").catch(e => console.log(e));
 }
 
-async function getPosts () {
-    const posts = (await list(ref(storage, "/project-posts"))).items;
-    for (const storage_reference of posts) {
-        getBlob(storage_reference)
-            .then(blob_result => blob_result.text())
-            .then(text_result => console.log(text_result));
+/**
+ * Converts markdown text to an HTML article.
+ * @param {string} markdown_text 
+ * @returns {HTMLElement} An article containing the converted markdown text.
+ */
+function getArticleFromMarkdown (markdown_text) {
+
+    const converter = new showdown.Converter();
+
+    const article = document.createElement("article");
+    article.innerHTML = converter.makeHtml(markdown_text);
+
+    return article
+}
+
+/**
+ * Converts markdown text to an HTML card. Assumes the first line starts with a single '#' character.
+ * @param {string} markdown_text 
+ * @returns {HTMLElement} A div containing information about the HTML text.
+ */
+function getCardFromMarkdown (markdown_text) {
+
+    const lines = markdown_text.split("\n");
+    const title = lines[0].slice(1, lines[0].length).trim();
+
+    const card = document.createElement("div");
+    card.class = "card";
+    card.textContent = title;
+
+    return card;
+}
+
+/**
+ * Retrieves posts from storage.
+ * @param {string} url The URL from which to retrieve the posts.
+ * @returns {Promise<string[]>}
+ */
+export async function getPosts(url) {
+
+    let texts = [];
+
+    /** @type {ListResult} */
+    const storage_references = await list(ref(storage, url))
+        .catch(e => { throw new Error(e) });
+
+    for (const ref of storage_references.items) {
+
+        const blob = await getBlob(ref)
+            .catch(e => { throw new Error(e) });
+
+        const text = await blob.text()
+            .catch(e => { throw new Error(e) });
+
+        getPageView().appendChild(getCardFromMarkdown(text));
     }
+
+    return texts;
 }
